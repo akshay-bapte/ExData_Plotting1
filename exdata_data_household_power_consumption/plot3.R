9f7e4d5d47088decb4d1ea0d03aa90ee4ce9aacb
Akshay Bapte <63445178+akshay-bapte@users.noreply.github.com>
2020-05-15 12:45:49 +0530

    Update plot3.R

--- exdata_data_household_power_consumption/plot3.R
+++ exdata_data_household_power_consumption/plot3.R
@@ -5,11 +5,11 @@ datetime <- paste(as.Date(data2use$Date), data2use$Time)
 data2use$Datetime <- as.POSIXct(datetime)
 with(data2use, {
      plot(Sub_metering_1~Datetime, type="l",
-                       ylab="Global Active Power (kilowatts)", xlab="")
+             +          ylab="Global Active Power (kilowatts)", xlab="")
        lines(Sub_metering_2~Datetime,col='Red')
        lines(Sub_metering_3~Datetime,col='Blue')
    })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
                  legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-png("plot3.png", width=480, height=480)
+dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
