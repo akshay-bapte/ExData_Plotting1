69b88b977785bb50534decd8d05fe80947ee9cf6
Akshay Bapte <63445178+akshay-bapte@users.noreply.github.com>
2020-05-15 12:45:02 +0530

    Update plot2.R

--- exdata_data_household_power_consumption/plot2.R
+++ exdata_data_household_power_consumption/plot2.R
@@ -7,5 +7,5 @@ with(data2use, {
        plot(Global_active_power~Datetime, type="l",
                        ylab="Global Active Power (kilowatts)", xlab="")
    })
-png("plot2.png", width=480, height=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
